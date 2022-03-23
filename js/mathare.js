f825f39f1df29fc459525c6850658dc3ebd50401
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-03-23 21:30:22 +0300

    ft;finished the hover effect

--- js/mathare.js
+++ js/mathare.js
@@ -5,4 +5,14 @@ $('.branch1').hover(function(){
     $(this).find('.brdisplay').toggle(500);
 });
 
+$('.brdisplay7').hide();
+$('.branch1').hover(function(){
+    $(this).find('.brdisplay7').toggle(500);
+});
+
+$('.brdisplay8').hide();
+$('.branch1').hover(function(){
+    $(this).find('.brdisplay8').toggle(500);
+});
+
 });
